9ef1be7306d4417f6258c3509378b20fd952cf5b
antfor <38752136+antfor@users.noreply.github.com>
2025-12-15 18:06:53 +0100

    getters for strength standards

--- lib/domain/standards/standards.dart
+++ lib/domain/standards/standards.dart
@@ -1,5 +1,9 @@
 
 
+import 'dart:math' as math;
+
+const eliteIndex = 4;
+
 enum Level {
   beginner(0.05),
   novice(0.2),
@@ -9,6 +13,20 @@ enum Level {
 
   final double percentage;
   const Level(this.percentage);
+
+  static Level byIndex(int i){
+    final index = math.min(math.max(0,i),eliteIndex);
+    return Level.values[index];
+  }
+  static Level next(Level lvl){
+    final i = Level.values.indexOf(lvl);
+    return byIndex(i+1);
+  }
+
+  static Level prev(Level lvl){
+    final i = Level.values.indexOf(lvl);
+    return byIndex(i-1);
+  }
 }
 
 enum Sex {
@@ -68,7 +86,7 @@ class StandardRatio {
 
   StandardRatio(this.id, this.beginner, this.novice, this.intermediate, this.advanced, this.elite);
 
-  double level(Level level) {
+  double levelRatio(Level level) {
     switch(level){
       case Level.beginner: return beginner;
       case Level.novice: return novice;
@@ -76,6 +94,20 @@ class StandardRatio {
       case Level.advanced: return advanced;
       case Level.elite: return elite;
     }
+  }
+
+  List<double> get ratios => [beginner, novice, intermediate, advanced, elite];
+    //0, 0.05, 0.2, 0.5, 0.8, 0.95, 1
+  
+
+  Level level(double bw, double lift){
+    final ratio = (lift/bw);
+    int index = ratios.indexWhere((r) =>  r < ratio );
+    if(index == -1){
+      return Level.elite;
+    }
+    index = math.max(0, index-1);
+    return Level.byIndex(index);
   }
 
 }
@@ -85,22 +117,36 @@ class StandardTable {
   final Iterable<int> bodyweight;
 
   final String id; //TODO: Exersise? or put in exersise
-  final Iterable<int> beginner;
-  final Iterable<int> novice;
-  final Iterable<int> intermediate;
-  final Iterable<int> advanced;
-  final Iterable<int> elite;
+  final List<int> beginner;
+  final List<int> novice;
+  final List<int> intermediate;
+  final List<int> advanced;
+  final List<int> elite;
 
   StandardTable(this.id, this.bodyweight, this.beginner, this.novice, this.intermediate, this.advanced, this.elite);
 
-  Iterable<int>  level(Level level) {
+  int value(double bw, Level level) {
+    int wclass = bodyweight.firstWhere((b) => bw <= b);
+    wclass = wclass == -1 ? bodyweight.last : wclass;
+
     switch(level){
-      case Level.beginner: return beginner;
-      case Level.novice: return novice;
-      case Level.intermediate: return intermediate;
-      case Level.advanced: return advanced;
-      case Level.elite: return elite;
+      case Level.beginner: return beginner[wclass];
+      case Level.novice: return novice[wclass];
+      case Level.intermediate: return intermediate[wclass];
+      case Level.advanced: return advanced[wclass];
+      case Level.elite: return elite[wclass];
     }
   }
 
+  List<int> standrads(int bw) => [beginner, novice, intermediate, advanced, elite].map((l)=>l.toList()[bw]).toList();
+  
+  Level level(double bw, double lift){
+    int wclass = bodyweight.firstWhere((b) => bw <= b);
+    wclass = wclass == -1 ? bodyweight.last : wclass;
+
+    int index = standrads(wclass).indexWhere((sd) => lift < sd);
+    index = index == -1 ? eliteIndex : index-1;
+
+    return Level.byIndex(index);
+  }
 }
