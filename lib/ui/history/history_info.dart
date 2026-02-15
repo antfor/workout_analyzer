82f88e986143f3c04300b98bed1f4c6cc3d9a14d
antfor <38752136+antfor@users.noreply.github.com>
2026-02-15 01:00:49 +0100

    add close button to history info and mad the size of the list constant

--- lib/ui/history/history_info.dart
+++ lib/ui/history/history_info.dart
@@ -9,19 +9,36 @@ import '/ui/util.dart' as util;
 class HistoryInfo extends StatelessWidget{
 
   final Workout workout;
+  final void Function() exit;
 
-  const HistoryInfo(this.workout, {super.key});
+  const HistoryInfo(this.workout, this.exit, {super.key});
   
   @override
   Widget build(BuildContext context) {
 
     final padding = EdgeInsetsGeometry.symmetric(vertical: 8, horizontal: 16);
     pad(w) => Container(padding:padding , child: w,);
-    final formatter = DateFormat('EEEE, MMM d, yyyy'); 
     
     return  ListView(children: [
-      pad(Column(crossAxisAlignment: CrossAxisAlignment.start, children: [util.H1(workout.title),Text(formatter.format(workout.startTime), style: TextStyle(color: Colors.grey,),)])),
+      pad(_header(workout, exit, context)),
     ]);
   }
 
+}
+
+Widget _header(Workout workout, void Function() exit, BuildContext context){
+
+  final formatter = DateFormat('EEEE, MMM d, yyyy'); 
+  final subHeadline =Text(formatter.format(workout.startTime), style: TextStyle(color: Colors.grey,),);
+
+  final headline =   Column(crossAxisAlignment: CrossAxisAlignment.start, 
+    children: [util.H1(workout.title),subHeadline]);
+  
+  final fontSize = Theme.of(context).textTheme.headlineLarge?.fontSize ?? 20;
+  final exitButton = IconButton(onPressed: exit, icon: Icon(Icons.close, size: fontSize));
+
+  return Row( 
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [headline, exitButton],);
 }
--- lib/ui/history/history_page.dart
+++ lib/ui/history/history_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import '/domain/info/lift_info.dart';
 import '/domain/workout.dart';
 import '/ui/app_page.dart';
 import '/ui/history/history.dart';
 import '/ui/history/history_info.dart';
 import '/ui/util.dart' as util;
 
-AppPage getHistoryPage(List<Workout> workouts, BuildContext context, {LiftBasicInfo? lift}){
-  return AppPage(title: _title, body: _HistoryState(lift, workouts), navIcon: _icon(context));
+AppPage getHistoryPage(List<Workout> workouts, BuildContext context){
+  return AppPage(title: _title, body: _HistoryState(workouts), navIcon: _icon(context));
 }
 
 NavigationDestination _icon(BuildContext context) => NavigationDestination(
@@ -19,10 +18,10 @@ NavigationDestination _icon(BuildContext context) => NavigationDestination(
 Widget _title = Text('History');
 
 class _HistoryState extends StatefulWidget{
- final LiftBasicInfo? lift;
+
  final List<Workout> workouts;
 
-  const _HistoryState(this.lift, this.workouts);
+  const _HistoryState(this.workouts);
 
   @override
   State<_HistoryState> createState() => _HistoryPage();
@@ -36,18 +35,21 @@ class _HistoryPage extends State<_HistoryState>  {
   @override
   Widget build(BuildContext context) {
 
-    final listPage = History(widget.lift, widget.workouts, (workout) => setState(() => selectedWorkout = workout));
+    final listPage = History(widget.workouts, selectedWorkout,(workout) => setState(() => selectedWorkout = workout));
 
     final loading = Text('');
-    final infoPage = selectedWorkout == null ? loading : HistoryInfo(selectedWorkout!);
+    void exit() => setState(() => selectedWorkout = null);
+    final infoPage = selectedWorkout == null ? loading : HistoryInfo(selectedWorkout!, exit);
 
     return  LayoutBuilder(
       builder: (context, constraints) {
         if(constraints.maxWidth > util.desktop){
           return Row(
             children: [
-              Flexible(flex: 10, child:listPage),
-              Flexible(flex: 9, child:infoPage)
+              Align( child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 600,), 
+                child: listPage)),
+              Expanded(child:infoPage)
             ],);
         }else if (constraints.maxWidth > util.tablet){
           
