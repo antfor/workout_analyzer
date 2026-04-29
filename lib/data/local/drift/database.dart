1b13efa270b5093a00532dcb2e22da5058b818e4
antfor <38752136+antfor@users.noreply.github.com>
2026-04-29 21:43:41 +0200

    stopped generating database in background

--- lib/data/local/drift/database.dart
+++ lib/data/local/drift/database.dart
@@ -5,7 +5,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:workout_analyzer/data/local/drift/seeding/static_data.dart';
 import 'package:workout_analyzer/data/local/drift/seeding/user_data.dart';
-
 import 'package:workout_analyzer/domain/domain.dart';
 import 'package:workout_analyzer/repository/import/import.dart';
 import 'package:workout_analyzer/repository/import/map.dart';
@@ -13,6 +12,8 @@ import 'package:workout_analyzer/repository/import/muscle.dart';
 import 'package:workout_analyzer/repository/import/standards.dart';
 
 part 'database.g.dart';
+
+typedef DdError = (Object,StackTrace)?;
 
 @DriftDatabase(
   // relative import for the drift file. Drift also supports `package:`
@@ -31,37 +32,42 @@ class AppDb extends _$AppDb {
   MigrationStrategy get migration => MigrationStrategy(
     onCreate: (m) async {
       await m.createAll();
-
+      try{
       // Seed default data //TODO do not neet to crate oject just create database directly
-      final muscles = await importMuscle();
-      final mapNames = await importMap();
-      final male = await importMale(mapNames);
-      final female = await importFemale(mapNames);
-      final Domain domain = await importMockData(muscles, male, female);
+      //ALSO handle this if seeding fail, just empty?
+        final muscles = await importMuscle();
+        final mapNames = await importMap();
+        final male = await importMale(mapNames);
+        final female = await importFemale(mapNames);
+        final Domain domain = await importMockData(muscles, male, female);
 
-      final (exercisesInserts, muscleInserts, equipmentInserts, swInserts) = staticData(domain);
+        final (exercisesInserts, muscleInserts, equipmentInserts, swInserts) = staticData(domain);
 
-      final (workoutsInserts,liftInserts,cardioInserts) = userData(domain.workouts);
-      print("seed");
-      await batch((b) {
+        final (workoutsInserts,liftInserts,cardioInserts) = userData(domain.workouts);
+        print("seed");
+        await batch((b) {
 
-        //STAIC DATA
-        b.insertAll(muscle, muscleInserts);
-        b.insertAll(equipment, equipmentInserts);
-        b.insertAll(standardsWeight, swInserts);
-        b.insertAll(exercises, exercisesInserts);
+          //STAIC DATA
+          b.insertAll(muscle, muscleInserts);
+          b.insertAll(equipment, equipmentInserts);
+          b.insertAll(standardsWeight, swInserts);
+          b.insertAll(exercises, exercisesInserts);
 
-        //USER DATA
-        b.insertAll(workouts, workoutsInserts);
-        b.insertAll(lift, liftInserts);
-        b.insertAll(cardio, cardioInserts); 
-      });
+          //USER DATA
+          b.insertAll(workouts, workoutsInserts);
+          b.insertAll(lift, liftInserts);
+          b.insertAll(cardio, cardioInserts); 
+        });
+      }catch (e, st) {
+        print("DB ERROR in seeding: $e");
+        print(st);
+      }
 
     },
   );
 
-  void import(Domain domain) async {
-
+  Future<DdError> import(Domain domain) async {
+    try{
       final (workoutsInserts,liftInserts,cardioInserts) = userData(domain.workouts);
       print("import");
       await batch((b) {
@@ -74,7 +80,14 @@ class AppDb extends _$AppDb {
         b.insertAll(workouts, workoutsInserts);
         b.insertAll(lift, liftInserts);
         b.insertAll(cardio, cardioInserts);
-    });
+      });
+    }catch (e, st) {
+      print("DB ERROR in import: $e");
+      print(st);
+      return (e, st);
+    }
+
+    return null;
   }
 
 }
@@ -87,7 +100,7 @@ LazyDatabase _openConnection() {
     final dbFolder = await getApplicationDocumentsDirectory();
     final file = File(p.join(dbFolder.path, 'db.sqlite'));
 
-    return NativeDatabase.createInBackground(file);
+    return NativeDatabase(file);
   });
 }
 
